e54ece794b3de44ea633a9fb62602d382d7c0ee2
吴小白 <296015668@qq.com>
2022-06-05 17:21:43 +0800

    fix: 修正命令错误

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 docker build -t dokcer-compose-loongarch64 .
-docker run --rm -v "${pwd}"/dist:/dist dokcer-compose-loongarch64
-ls -al "${pwd}"/dist
+docker run --rm -v "$(pwd)"/dist:/dist dokcer-compose-loongarch64
+ls -al "$(pwd)"/dist
